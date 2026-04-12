377215bcae09b8c1598852d8220f37a6321f3ba7
Lucca Gabriel <luccagabriel12@hotmail.com>
2026-04-12 18:26:13 -0300

    card

--- src/components/real_estate_card/index.tsx
+++ src/components/real_estate_card/index.tsx
@@ -1,10 +1,7 @@
-/* eslint-disable @next/next/no-img-element */
 /* eslint-disable @typescript-eslint/no-unsafe-function-type */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
-// Next
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 // Store
 import { RealEstate } from "@/store/real_estate";
@@ -12,19 +9,24 @@ import { useRealEstateStore } from "@/store";
 // Components
 import { Card } from "@/components/ui/card";
 // Icons
-import { FaBed, FaBath, FaBookmark, FaRegBookmark } from "react-icons/fa";
-import { FaLocationDot } from "react-icons/fa6";
+import { MdOutlineBed, MdOutlineShower } from "react-icons/md";
+import { FaLocationPin } from "react-icons/fa6";
 import { PiGarage } from "react-icons/pi";
-import { GiExpand } from "react-icons/gi";
+import { BiArea } from "react-icons/bi";
 import { MdVerified } from "react-icons/md";
 
 export default function RealEstateCard({ realEstate, onClickCallback }: { realEstate: RealEstate; onClickCallback?: Function }) {
+  const { realEstateSelected, setRealEstateSelected } = useRealEstateStore((state) => state);
   const router = useRouter();
 
-  const { realEstateSelected, setRealEstateSelected } = useRealEstateStore((state) => state);
-  const [isFavorited, setIsFavorited] = useState(false);
-
   const isSelected = realEstateSelected?._id === realEstate._id;
+
+  const handleType = (type: string) => {
+    if (type === "house") return "Casa";
+    if (type === "apartment") return "Apartamento";
+    if (type === "land") return "Terreno";
+    if (type === "sobrado") return "Sobrado";
+  };
 
   const handleCardClick = () => {
     if (onClickCallback) onClickCallback();
@@ -34,11 +36,6 @@ export default function RealEstateCard({ realEstate, onClickCallback }: { realEs
     }
 
     setRealEstateSelected(realEstate);
-  };
-
-  const handleFavoriteClick = (event: any) => {
-    event.stopPropagation();
-    setIsFavorited(!isFavorited);
   };
 
   return (
@@ -52,50 +49,50 @@ export default function RealEstateCard({ realEstate, onClickCallback }: { realEs
           <MdVerified size={18} color="white" className="ml-1 mt-1" />
         </div>
       )}
-
       {/* Imagem */}
       <div className={`h-[22rem] w-full rounded-[0.8rem] relative`}>
         <img className={`h-[100%] w-[100%] rounded-[0.8rem] object-cover object-center`} src={realEstate.thumbnail} alt="" />
-
-        <div className="absolute bottom-4 right-4" onClick={handleFavoriteClick}>
-          {isFavorited ? <FaBookmark color="red" /> : <FaRegBookmark color="red" />}
-        </div>
       </div>
       <div className="grow px-5 pt-3 pb-2 bg-white">
         {/* Title */}
         <div className="flex justify-between items-center">
           <span className="text-[2.6rem] font-extrabold">R$ {realEstate.price.toLocaleString()}</span>
-          <span className="text-[1.4rem] italic text-gray-600">{realEstate.type}</span>
+          <span className="text-[1.4rem] italic text-gray-600">{handleType(realEstate.type)}</span>
         </div>
         {/* Body */}
-        <div className="flex justify-between text-[1.6rem]">
-          <div className="flex items-center">
-            <FaBed size={16} color="text-foreground" className="mr-2" />
-            <span className="font-bold text-[1.6rem] mr-1 ">{realEstate.rooms}</span>
-            <span className="">quartos</span>
-          </div>
-          <div className="flex items-center">
-            <FaBath size={16} color="text-foreground" className="mr-2" />
-            <span className="font-bold text-[1.6rem] mr-1">{realEstate.bathrooms}</span>
-            <span className="">banheiros</span>
-          </div>
-          <div className="flex items-center">
-            <PiGarage size={16} color="text-foreground" className="mr-2" />
-            <span className="font-bold text-[1.6rem] mr-1">{realEstate.garages}</span>
-            <span>garagem</span>
-          </div>
-          <div className="flex items-center">
-            <GiExpand size={16} color="text-foreground" className="mr-2" />
-            <span className="font-bold text-[1.6rem] mr-1">{realEstate.area}</span>
-            <span>
-              m<sup>2</sup>
-            </span>
-          </div>
+        <div className="flex items-center gap-[2rem]">
+          {realEstate.rooms > 0 && (
+            <div className="h-[2rem] flex items-center">
+              <MdOutlineBed size={16} color="#6c757d" className="mr-2" />
+              <span className="font-bold text-[1.5rem] text-[#6c757d] mr-1">{realEstate.rooms}</span>
+              <span className="font-bold text-[1.5rem] text-[#6c757d]">quartos</span>
+            </div>
+          )}
+          {realEstate.bathrooms > 0 && (
+            <div className="h-[2rem] flex items-center">
+              <MdOutlineShower size={16} color="#6c757d" className="mr-2" />
+              <span className="font-bold text-[1.5rem] text-[#6c757d] mr-1">{realEstate.bathrooms}</span>
+              <span className="font-bold text-[1.5rem] text-[#6c757d]">banheiros</span>
+            </div>
+          )}
+          {realEstate.garages > 0 && (
+            <div className="h-[2rem] flex items-center">
+              <PiGarage size={16} color="#6c757d" className="mr-2" />
+              <span className="font-bold text-[1.5rem] text-[#6c757d] mr-1">{realEstate.garages}</span>
+              <span className="font-bold text-[1.5rem] text-[#6c757d]">vaga</span>
+            </div>
+          )}
+          {realEstate.area > 0 && (
+            <div className="h-[2rem] flex items-center">
+              <BiArea size={16} color="#6c757d" className="mr-2" style={{ marginBottom: "1px" }} />
+              <span className="font-bold text-[1.5rem] text-[#6c757d]">{realEstate.area} m²</span>
+            </div>
+          )}
         </div>
         {/* Footer */}
-        <div className="flex items-center select-text mt-4">
-          <FaLocationDot size={14} color="red" className="mr-2" />
-          <span className="text-[1.4rem] italic leading-6">
+        <div className="flex items-center select-text mt-3">
+          <FaLocationPin size={12} color="#c53030" className="mr-2" style={{ marginBottom: "1px" }} />
+          <span className="text-[#6c757d] text-[1.4rem] italic leading-6">
             {realEstate?.address.street}, {realEstate?.address.number}, {realEstate?.address.district}, {realEstate?.address.city},{" "}
             {realEstate?.address.state}
           </span>
